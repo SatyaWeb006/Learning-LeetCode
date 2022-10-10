a3090f3fd5e409956402af7246d8f5a529f734cb
Syuan Tsai <carsun00@gmail.com>
2022-10-10 21:54:45 +0800

    Feat 新增
    ThreeLetters 解法 1.0

---
+++ codility.com/ThreeLetters/ThreeLetters.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace codility.com.ThreeLetters
+{
+    public class Solution
+    {
+        /// <summary>
+        ///     透過正則表示法處理
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public int solution(string s)
+        {
+            var result = SolutionRegex(s);
+            return result;
+        }
+
+
+        /// <summary>
+        ///     第一種解法；使用正則表示法
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private int SolutionRegex(string str)
+        {
+            var regex = new Regex("a{3,}|b{3,}");
+
+            var result = regex.Matches(str);
+
+            return result.Count;
+        }
+    }
+}
---
+++ codility.com/ThreeLetters/ThreeLettersTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+// ReSharper disable StringLiteralTypo
+
+namespace codility.com.ThreeLetters
+{
+    [TestFixture]
+    public class ThreeLettersTest
+    {
+        [TestCase("baaaaa", 1)]
+        [TestCase("baaabbaabbba", 2)]
+        [TestCase("baabab", 0)]
+        public void Example1(string nums, int assert)
+        {
+            var time = Stopwatch.StartNew();
+            var answer = new Solution();
+            var result = answer.solution(nums);
+            time.Stop();
+            var times = time.ElapsedMilliseconds;
+
+            Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().Be(assert, "預期結果不一致。");
+            times.Should().BeLessThan(1000);
+        }
+    }
+}
