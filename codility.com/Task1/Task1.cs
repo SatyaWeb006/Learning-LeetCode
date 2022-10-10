f94b442acd53d8e3c88c80c9684da88444c3e19d
Syuan Tsai <carsun00@gmail.com>
2022-10-10 14:17:03 +0800

    Feat : 新增
    Demo Task1的題目

---
+++ codility.com/Task1/Task1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace codility.com.Task1
+{
+    // you can also use other imports, for example:
+    // using System.Collections.Generic;
+
+    // you can write to stdout for debugging purposes, e.g.
+    // Console.WriteLine("this is a debug message");
+    //  解析筆記：https://www.notion.so/syuantsai/1-8ecfb765c2d2446caa3b9f0049a6c691
+    //  GitLab ：https://gitlab.com/SyuanTsai/Learning/LeetCode.git
+    
+    public class Solution
+    {
+        /// <summary>
+        ///     不先產生雜湊表，
+        ///     反向過來透過來源資料生產雜湊表，
+        ///     再逐一比對缺少的元素。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public int solution(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (num < 0) continue;
+                if (dic.TryGetValue(num, out _))
+                {
+                    continue;
+                }
+
+                dic.Add(num, num);
+            }
+
+            for (var i = 1; i <= 10000; i++)
+            {
+                if (dic.TryGetValue(i, out _))
+                {
+                    continue;
+                }
+
+                return i;
+            }
+
+            throw new ArgumentNullException(nameof(nums), "Out Of Range.");
+        }
+
+
+        /// <summary>
+        ///     直接生出所有的資料列，
+        ///     排除掉已經有的資料後取最小值。
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int 暴力解v1(int[] a)
+        {
+            var hash = new List<int>();
+            for (int i = 1; i <= 10000; i++)
+            {
+                hash.Add(i);
+            }
+
+            foreach (var num in a)
+            {
+                hash.Remove(num);
+            }
+
+            return hash.Min();
+        }
+    }
+}
---
+++ codility.com/Task1/Task1Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace codility.com.Task1
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [TestCase(new int[] {1, 3, 6, 4, 1, 2}, 5)]
+        [TestCase(new int[] {1,2,3},  4)]
+        [TestCase(new int[] {-1,-3},  1)]
+        public void Example1(int[] nums, int assert)
+        {
+            var time =  Stopwatch.StartNew();
+            var answer = new Solution();
+            var result = answer.solution(nums);
+            time.Stop();
+            var times = time.ElapsedMilliseconds;
+            
+            Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().Be(assert, "預期結果不一致。");
+            times.Should().BeLessThan(1000);
+        }
+    }
+}
