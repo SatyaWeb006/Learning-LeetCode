d26cfd50e3c9b7fc882bab85cb0ab9da565a0c3c
Syuan Tsai <carsun00@gmail.com>
2022-11-17 19:20:38 +0800

    Feat Add
    PalindromeNumber

---
+++ LeetCode.com/00009_Palindrome Number/PalindromeNumber.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.com._00009_Palindrome_Number
+{
+    /// <summary>
+    ///     Given an integer `x`, return `true` if `x` is a ***palindrome***, and `false` otherwise.
+    ///     給予一個整數X，如果是[回文]回傳True，否則回傳Falae
+    /// 
+    ///     LeetCode ；https://leetcode.com/problems/palindrome-number/
+    /// </summary>
+    public static class Solution
+    {
+        /// <summary>
+        ///     解題思路 : https://www.notion.so/syuantsai/00009-Palindrome-Number-53f39ff3247b488e8681220cf68c8d44
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static bool IsPalindrome(int x)
+        {
+            var input = x.ToString().ToCharArray();
+            
+            if (input[0] ==  '-')
+            {
+                return false;
+            }
+
+            int maxIndex = input.Count();
+            for (int i = 0; i < maxIndex/2 ; i++)
+            {
+                if (input[i] == input[maxIndex])
+                {
+                    maxIndex--;
+                    continue;
+                }
+                
+                
+            }
+            
+            
+            return true;
+        }
+    }
+}
---
+++ LeetCode.com/00009_Palindrome Number/PalindromeNumberTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCode.com._00009_Palindrome_Number
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [TestCase(121, true)]
+        [TestCase(-121, false)]
+        [TestCase(10, false)]
+        public void Example1(int nums, bool assert)
+        {
+            //var time = Stopwatch.StartNew();
+            var result = Solution.IsPalindrome(nums);
+            //time.Stop();
+            //var times = time.ElapsedMilliseconds;
+            //Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().Be(assert, "預期結果不一致。");
+            //times.Should().BeLessThan(1000);
+        }
+    }
+}
