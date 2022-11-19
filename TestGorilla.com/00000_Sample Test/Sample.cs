c4a2f3d1d9a617852748e3ee36da72444b4428db
Syuan Tsai <carsun00@gmail.com>
2022-11-19 16:27:06 +0800

    Feat 新增
    TestGroilla.com

---
+++ TestGorilla.com/00000_Sample Test/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace TestGorilla.com._00000_Sample_Test
+{
+    public static class Sample
+    {
+        public static string ConvertFrom12To24HoursFormat(string inputTime)
+        {
+            DateTime.TryParse(inputTime, out var time);
+            return time.ToString("H:mm", CultureInfo.CurrentCulture);
+        }
+    }
+}
---
+++ TestGorilla.com/00000_Sample Test/SampleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestGorilla.com._00000_Sample_Test
+{
+    
+    [TestFixture]
+    public class SampleTest
+    {
+        [TestCase("12:00 am","0:00")]
+        [TestCase("11:00 pm","23:00")]
+        public void Example1(string inputTime, string outTime)
+        {
+            var time =  Stopwatch.StartNew();
+            var result = Sample.ConvertFrom12To24HoursFormat(inputTime);
+            time.Stop();
+            var times = time.ElapsedMilliseconds;
+            Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().BeEquivalentTo(outTime, "預期結果不一致。");
+        }
+    }
+}
