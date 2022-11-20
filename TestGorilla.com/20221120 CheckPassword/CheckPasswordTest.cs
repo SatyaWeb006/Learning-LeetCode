91be7e3f2cb8b1735192912d1c39ce963ed11acd
SyuanTsai <carsun00@gmail.com>
2022-11-20 18:47:57 +0800

    Feat 測驗
    Case Two

---
+++ TestGorilla.com/20221120 CheckPassword/CheckPasswordTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestGorilla.com._20221120_CheckPassword
+{
+    [TestFixture]
+    public class CheckPasswordTest
+    {
+        [TestCase(new[] {"P@sswORD1", "20passWORD20", "PASS6word"}, "4dro6", "PASS6word")]
+        [TestCase(new[] {"theBestpassword", "myPassword#3"}, "1dro0", "theBestpassword")]
+        [TestCase(new[] {"MyPassword2", "crazyPassword1#$%"}, "1%$#1", "crazyPassword1#$%")]
+        [TestCase(new[] {"luckyPASSWORD#$$$", "luckyDat666#3"}, "6pass7", "")]
+        public void Example1(string[] input, string checkData, string outData)
+        {
+            var time = Stopwatch.StartNew();
+            var result = Password.CheckPassword(input, checkData);
+            time.Stop();
+            var times = time.ElapsedMilliseconds;
+            Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().Be(outData, "預期結果不一致。");
+        }
+
+
+        [TestCase(new[] {"PASS6word"}, "4dro6", "PASS6word")]
+        public void Example2(string[] input, string checkData, string outData)
+        {
+            var time = Stopwatch.StartNew();
+            var result = Password.CheckPassword(input, checkData);
+            time.Stop();
+            var times = time.ElapsedMilliseconds;
+            Console.WriteLine(time.ElapsedMilliseconds);
+            result.Should().Be(outData, "預期結果不一致。");
+        }
+    }
+}
---
+++ TestGorilla.com/20221120 CheckPassword/Password.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TestGorilla.com._20221120_CheckPassword
+{
+    public static class Password
+    {
+        public static string CheckPassword(string[] input, string check)
+        {
+            foreach (var t in input)
+            {
+                if (PasswordDetail(t, check))
+                {
+                    return t;
+                }
+            }
+
+            return "";
+        }
+
+        private static bool PasswordDetail(string input, string check)
+        {
+            int.TryParse(check.Substring(0, 1), out var upCase);
+            var srcFinal = check.Substring(1, 3);
+            var lastStr = check.LastIndexOf(srcFinal, StringComparison.Ordinal) + 3;
+            var finalStr = new string(srcFinal.Reverse().ToArray()).Replace("$", "\\$");
+            var lastLen = check.Length - lastStr;
+            int.TryParse(check.Substring(lastStr, lastLen), out var numSum);
+
+            if (Regex.Matches(input, "[A-Z]").Count != upCase)
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(input, $"^.*{finalStr}"))
+            {
+                return false;
+            }
+
+            var sum = 0;
+            foreach (var str in input)
+            {
+                if (int.TryParse(str.ToString(), out var num))
+                {
+                    sum += num;
+                }
+            }
+
+            return numSum == sum;
+        }
+    }
+}
