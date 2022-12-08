df8c0a88948ce9593d09c807197b6dd94095d0c8
Syuan Tsai <carsun00@gmail.com>
2022-12-08 21:45:12 +0800

    Feat 建立範本

---
+++ LeetCode.com/00000_Template/Template.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.com._00000_Template
+{
+    /// <summary>
+    /// </summary>
+    public static class Solution
+    {
+        /// <summary>
+        ///     解題思路 : https://www.notion.so/syuantsai/2a52eed4b34f4e00a1e49e96a66f6963
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] Template(int[] nums, int target)
+        {
+            return null;
+        }
+
+    }
+}
